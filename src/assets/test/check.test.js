c58c6e78a4c2e37f5e27d7c7e84def529b182b99
Leroy aurelien <83220559+aurelienLRY@users.noreply.github.com>
2024-02-09 21:05:59 +0100

    Update check.test.js

--- src/assets/test/check.test.js
+++ src/assets/test/check.test.js
@@ -3,7 +3,7 @@ import { describe, it , expect } from "vitest";
 describe('Verification de l\'environement de test ', () => {
     
 it('Doit retourner true', () => {
-expect(true).toBe(false);
+expect(true).toBe(true);
   });
   
 it('Doit retourner false', () => {
